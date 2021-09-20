bfb840198a0d06f11308876e4a71930b47ee17ea
rb-rishika <88501367+rb-rishika@users.noreply.github.com>
2021-09-20 15:10:13 -0400

    Add files via upload

--- aStar_rb.py
+++ aStar_rb.py
@@ -1,7 +1,18 @@
 infinity = float('infinity')
 
+def get_minimum (unvisited):
+	return min(unvisited, key=lambda k:float(unvisited[k][0]))
+
 def heurisitic(x1,y1,x2,y2): #Manhattan Distance
 	return abs(int(x1)-int(x2))+abs(int(y1)-int(y2))
+
+def get_params(curr, neigh): 
+	x1=curr[1]
+	y1=curr[3]
+	x2=neigh[1]
+	y2=neigh[3]
+	return x1,y1,x2,y2
+
 #nested dicts
 graph = {   #g(n)
 		   '(0,0)': {'(0,1)':10, '(1,0)':12},
@@ -33,33 +44,50 @@ print(graph)
 start_node='(0,0)'
 target_node='(3,7)'
 
-
-
-def astar(graph, start_node, target_node):
-	G_score=0
-	F_score=1
-	Previous=2
+def astar(graph, start_node, target_node): 
+	l_keys = list(graph.keys())
+	G_SCORE=0
+	F_SCORE=1
+	PREVIOUS=2
 
 	visited ={}    #empty dictionaries
 	unvisited={}   #nested dictionaries
 
 	for key in graph:
-		unvisited[key]=[infinity , infinity , None]   #(f(n, g(n).parent node))
-	print(unvisited) #end of for 
+		unvisited[key]=[infinity , infinity , None]   #(g(n), f(n), parent node)), copying entire graph in unvisited 
 
 	h_score = heurisitic(l_keys[0][1],l_keys[0][3], l_keys[23][1], l_keys[23][3])
 	print(h_score)
 
-	unvisited[start_node] = [0, h_score, NULL]  
+	unvisited[start_node] = [0, h_score, None]  
+	print (unvisited)
 	finished = False
 
 	#ASTAR START
-	While (finished == False):
+	while (finished == False):
 		if(len(unvisited)==0):
 			finished= True
 		else:
-			
+			current_node=get_minimum(unvisited)
+			if (current_node==target_node):
+				finished=True
+				visited[current_node] = unvisited[current_node]   #End of if
+			else: 
+				for neigh in graph[current_node]:
+					#we will check only UNVISITED NODES
+					if neigh not in visited:     # Calculate new g-score
+						new_g_score = unvisited[current_node][G_SCORE] + graph[current_node][neigh]
+						if new_g_score < unvisited[neigh][G_SCORE]: 
+							#updation 
+							unvisited[neigh][G_SCORE] = new_g_score
+							x1,y1,x2,y2=get_params(current_node, neigh)
+							unvisited[neigh][F_SCORE] = new_g_score + heurisitic(x1,y1,x2,y2)
+							unvisited[neigh][PREVIOUS] = current_node #end of inner if 
+							#end of outer if
+				visited[current_node] = unvisited[current_node] 		# Add current node to visited list
+				del unvisited[current_node]
+		                # Remove from unvisited list
+	return visited
 
-
-
+#end of astar function 
 astar(graph, '(0,0)', '(3,7)')
--- heuristic.py
+++ heuristic.py
@@ -4,33 +4,41 @@ def heurisitic(x1,y1,x2,y2): #Manhattan Distance
     print(type(x1))
     return abs(int(x1)-int(x2))+abs(int(y1)-int(y2))
 
-graph={'(0,0)': {'(0,1)':1, '(0,2)':2, '(0,3)':3}, #assume this is a start node 
-           '(0,1)': {'(0,3)':4, '(5,4)':5},    #assume this is the goal node
+graph={'(A)': {'g(n)':17, '(0,2)':2, '(0,3)':3}, #assume this is a start node 
+           '(B)': {'g(n)':4, '(5,4)':5},    #assume this is the goal node
 
 }
 
 
 l_keys = list(graph.keys())
-l_values=list(graph.values())
+l_values=graph.values()
 #l_values_values=list(l_values.values())
 
 #print(type(heurisitic(l_keys[0][1],l_keys[0][3], l_keys[1][1], l_keys[1][3])))
 
 #def minimum(graph):
 
-for j in l_values:
-    print(min(j.values())
-    #if(j.values()==min):
-        #print(l_values.key())
 '''
-    print(j.keys(), min(j.values()))
-    if(min<min())'''
-    #res = [key for j in l_values if l_values[j] == min]
-    #print(res)
+for i,j in graph.items():
+    print(min(j.values()))
+    for i in j:
+        if (min(i.values())):
+            print(i)
+   
+'''
+for i in l_values:
+    print (min(i.values()))
+print(min(graph, key=lambda k:float(graph[k]['g(n)'])))
+
+#print(graph['(0,1)'])
+
+#unvisited ={'(0,1)': g(), f(), nn}
+
+neigh= '(0,1)'
+
+#def get_params(neigh): 
+print(neigh[1])
 
 
-    '''
-    for k in j:
-        print(j[k])
-    #print(l_values[i][2])
-  '''
+
+
